52166a30061e8c64d956cf6f6041137ad8262af4
Derek Johnson <siestaderek@gmail.com>
2020-09-13 20:39:10 -0600

    added signIn component

---
+++ pages/signIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <SignIn />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
